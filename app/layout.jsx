5d858182180e402bc8433b1e8b215e6bafd5c8f1
Abdullah Ajayi <hafizajayi2308@gmail.com>
2024-07-01 17:25:57 +0100

    updated app icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,7 +9,7 @@ const layout = ({ children }) => {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/file-text.svg" />
+        <link rel="icon" href="/assets/brainwave-symbol.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body suppressHydrationWarning={true}>{children}</body>
