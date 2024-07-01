c85c276486ad40d54bf33f8aeea6ce3bb310f38d
Abdullah Ajayi <hafizajayi2308@gmail.com>
2024-07-01 17:40:00 +0100

    updated app's preview image

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,7 @@ const layout = ({ children }) => {
       <head>
         <link rel="icon" href="/assets/brainwave-symbol.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:image" content="/assets/4-small.png" />
+        <meta property="og:image" content="/background.jpg" />
       </head>
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
